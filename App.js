ac2414564278cd92f2de0a420ac5d83fbc054f2a
Victor <adevic4christ@gmail.com>
2020-02-12 15:47:27 +0100

    Loaded nunito fonts in App.js

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { AppLoading } from "expo";
 import * as Font from "expo-font";
 import Home from "./screens/home";
 
@@ -9,5 +10,13 @@ const getFonts = () =>
  });
 
 export default function App() {
- return <Home />;
+ const [fontsLoaded, setFontsLoaded] = useState(false);
+
+ if (fontsLoaded) {
+  return <Home />;
+ } else {
+  return (
+   <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
+  );
+ }
 }
