12c2b63f14763012a5a22d46525b539824332447
Victor <adevic4christ@gmail.com>
2020-02-12 14:34:21 +0100

    Created screens folder, home files, about and reviewDetails files

---
+++ screens/home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export default function Home() {
+ return (
+  <View style={styles.container}>
+   <Text>Home Screen</Text>
+  </View>
+ );
+}
