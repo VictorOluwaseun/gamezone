4a3f5eab8a2482901b9563a338884ca21d30c7c2
Victor <adevic4christ@gmail.com>
2020-02-12 15:30:01 +0100

    Created getFonts function

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React from "react";
+import * as Font from "expo-font";
 import Home from "./screens/home";
+
+const getFonts = () =>
+ Font.loadAsync({
+  "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+  "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf")
+ });
 
 export default function App() {
  return <Home />;
