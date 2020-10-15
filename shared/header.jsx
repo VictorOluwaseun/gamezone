f8121fccc8909075503d0fa669f3c8ea01f65aa6
Victor <adevic4christ@gmail.com>
2020-10-15 16:10:22 +0100

    ready to push

--- shared/header.jsx
+++ shared/header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
 export default function Header({ navigation, title }) {
@@ -8,17 +8,24 @@ export default function Header({ navigation, title }) {
  };
 
  return (
-  <View style={styles.header}>
+  <ImageBackground
+   source={require("./../assets/game_bg.png")}
+   style={styles.header}
+  >
    <MaterialIcons
     name="menu"
     size={28}
     onPress={openMenu}
     style={styles.icon}
    />
-   <View>
+   <View style={styles.headerTitle}>
+    <Image
+     source={require("./../assets/heart_logo.png")}
+     style={styles.headerImage}
+    />
     <Text style={styles.headerText}>{title}</Text>
    </View>
-  </View>
+  </ImageBackground>
  );
 }
 
@@ -28,7 +35,7 @@ const styles = StyleSheet.create({
   height: "100%",
   flexDirection: "row",
   alignItems: "center",
-  alignContent: "center",
+  // alignContent: "center",
   justifyContent: "center",
   paddingHorizontal: 105
  },
@@ -42,5 +49,13 @@ const styles = StyleSheet.create({
  icon: {
   position: "absolute",
   left: 0
+ },
+ headerTitle: {
+  flexDirection: "row"
+ },
+ headerImage: {
+  width: 26,
+  height: 26,
+  marginHorizontal: 10
  }
 });
